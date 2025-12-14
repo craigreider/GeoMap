06abdfa5d9b3f5e623aa11a453316fd340151a06
craigreider <craigreider@yahoo.com>
2025-12-13 21:30:35 -0500

    add as_uri and renamed

--- geo_map.py
+++ geo_map.py
@@ -5,16 +5,13 @@ import json
 import web_browser_wrapper
 from pathlib import Path
 
-# output html folder
-html_folder = Path("C:/Code/GeoMap/html")
-html_file = html_folder / "geocoded_map.html"
-# Build file:// URL automatically
-file_url = html_file.as_uri()
 # Load dataset
 data = pd.read_excel("data.xlsx")
 geolocator = Nominatim(
     user_agent="distance_calculator(geo-map craigreider@yahoo.com)", timeout=10
 )
+
+print("after: geolocator")
 
 
 def geocode_address(row):  # -> tuple | tuple[None, None]:
@@ -39,7 +36,7 @@ data["Latitude"], data["Longitude"] = zip(*data.apply(geocode_address, axis=1))
 map_center = [data["Latitude"].mean(), data["Longitude"].mean()]
 
 # make map with folium
-mymap = folium.Map(location=map_center, zoom_start=7)  # adjust as needed
+folium_map = folium.Map(location=map_center, zoom_start=7)  # adjust as needed
 
 # Add markers
 for index, row in data.iterrows():
@@ -47,7 +44,7 @@ for index, row in data.iterrows():
         popup_text = f"{row['City']}<br>Population: {row['City']}"
         # popup_text=f"{row["City"]}"
         folium.Marker([row["Latitude"], row["Longitude"]], popup=popup_text).add_to(
-            mymap
+            folium_map
         )
 
 # Load GeoJSON data for the state (replace 'state_geojson.json' with your file)
@@ -64,10 +61,15 @@ folium.GeoJson(
         "weight": 1,
         "fillOpacity": 0.05,
     },
-).add_to(mymap)
+).add_to(folium_map)
 
 # Add a layer control and display the map
-folium.LayerControl().add_to(mymap)
+folium.LayerControl().add_to(folium_map)
+# output html folder
+html_file = "geocoded_map.html"
+html_folder = Path("C:/Code/GeoMap/html")
+html_path = html_folder / html_file
+html_local_uri = html_path.as_uri()  # Build file:// URL automatically
 # Save Folium map
-mymap.save(html_file)
-web_browser_wrapper.open_file_in_edge(file_url, 1)
+folium_map.save(html_path)
+web_browser_wrapper.open_file_in_edge(html_local_uri, 1)
